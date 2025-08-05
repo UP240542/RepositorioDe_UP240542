6b499f5d855897c41a90fe84754d86e7218425ef
UP240542 <orl34969@gmail.com>
2025-08-04 19:14:23 -0600

    Update Dia8.py

--- up240542/Dia8.py
+++ up240542/Dia8.py
@@ -1,20 +1,55 @@
-dog = {}
-dog['nombre'] = 'Rex'
-dog['edad'] = 5
-dog['raza'] = 'Labrador'
-print(dog, len(dog))
+# Crear un diccionario vacío
+perro = {}
 
-student = {
-    'first_name': 'Ana', 'last_name': 'Lopez',
-    'edad': 22, 'skills': ['Python','SQL'], 'ciudad': 'CDMX'
+# Añadir elementos al diccionario
+perro['nombre'] = 'Firulais'
+perro['color'] = 'marrón'
+perro['raza'] = 'labrador'
+perro['patas'] = 4
+perro['edad'] = 5
+print(perro)
+
+#3. Cree un diccionario de estudiantes y agregue nombre, apellido, género, edad, estado civil, habilidades, país, ciudad y dirección como claves para el diccionario.
+estudiantes = {
+    'Nombre': 'Graciela',
+    'Apellido': 'Barcelata',
+    'Genero': 'Femenino',
+    'Edad': '18',
+    'Estado civil': 'Soltera',
+    'Habilidades': ['Trabajo en equipo', 'Excel'],
+    'Pais': 'México',
+    'Ciudad': 'Aguascalientes',
+    'Dirección': 'Av. Revolución 123, Col. Centro'
 }
-print(student.keys(), student.values())
-student['skills'].append('Django')
-print(student['skills'])
+print(estudiantes)
 
-items = list(student.items())
-print(items)
-del student['ciudad']
-copy = student.copy()
-student.clear()
-print(student, copy)
+#4. Obtenga la longitud del diccionario del estudiante
+print(len(estudiantes))
+
+#5. Obtenga el valor de las habilidades y verifique el tipo de dato, debe ser una lista
+habilidades = estudiantes['Habilidades']
+print('Habilidades: ', habilidades)
+print(type(habilidades))
+
+#6. Modifique los valores de las habilidades agregando una o dos habilidades
+estudiantes['Habilidades'] = 'Trabajo en equipo','Excel','Patinar'
+print(estudiantes)
+
+#7. Obtenga las claves del diccionario como una lista
+claves = estudiantes.keys()
+print(claves)
+
+#8 .Obtener los valores del diccionario como una lista
+valores = estudiantes.values()
+print(valores)
+
+#9. Cambie el diccionario a una lista de tuplas usando el método items()
+print(estudiantes.items())
+
+#10. Eliminar uno de los elementos del diccionario
+estudiantes.pop('Dirección')
+print(estudiantes)
+
+#11. Eliminar uno de los diccionarios
+print(perro)
+del perro
